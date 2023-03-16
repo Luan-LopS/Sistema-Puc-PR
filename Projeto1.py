f80cd410eb947a6bd939b69b9f5cfeee5cf98aa5
Luan <luam10@hotmail.com.br>
2023-03-16 11:02:00 -0300

    initial commit

---
+++ Projeto1.py
@@ -0,0 +1,18 @@
+# Projeto sistema de gerenciamento academico 
+#trabalho do semestre 
+
+print("Seja bem vindo ao sistema PUC-PR")
+
+
+# Sistema principal do sistema 
+def main():
+    print("\n-----------SISTEMA PUC-PR--------------")
+    print("____________MENU INICIAL________________")
+
+
+
+
+
+# Sitema 
+
+main()
