00a251a7961ee4f6278f847af4eb056de5174cbd
Luan <luam10@hotmail.com.br>
2023-03-17 00:08:56 -0300

    Desenvolvimento de estruturas de repetição.

--- Projeto1.py
+++ Projeto1.py
@@ -1,51 +1,61 @@
 # Projeto sistema de gerenciamento academico 
 #trabalho do semestre 
 
-print("----------Seja bem vindo ao sistema PUC-PR----------\n\n")
+print("\n\n----------Seja bem vindo ao sistema PUC-PR----------\n")
 
-#main
-print("\n-----------MENU INICIAL--------------")
-print('Escolha uma da opções abaixo para gerenciar: \n' 
-          ' 1) Alunos\n'
-          ' 2) Professores\n'
-          ' 3) Disciplinas\n'
-          ' 4) Turmas\n'
-          ' 5) Matriculas\n'
-          ' 0) Sair do sitema') 
-opcao = int(input())
-
-if opcao == 1:
-#Alunos
-    print("\n=============Alunos================")
-    print('Escolha uma da opções abaixo:\n' 
-          ' 1) Incluir\n'
-          ' 2) Listar\n'
-          ' 3) Excluir\n'
-          ' 4) Alterar\n'
-          ' 0) Menu inicial\n')
+opcao=1
+while opcao !=0:
+    #main
+    print("\n-----------MENU INICIAL--------------")
+    print('Escolha uma da opções abaixo para gerenciar: \n' 
+            ' 1) Alunos\n'
+            ' 2) Professores\n'
+            ' 3) Disciplinas\n'
+            ' 4) Turmas\n'
+            ' 5) Matriculas\n'
+            ' 0) Sair do sitema') 
     opcao = int(input())
+
     if opcao == 1:
-        print("------------INCLUIR-----------")
-        print('EM DESENVOLVIMENTO.......\n'
-              'FIM...')
+    #Alunos
+        print("\n=============Alunos================")
     elif opcao == 2:
-        print("------------LIstar-----------")
-        print('EM DESENVOLVIMENTO.......\n'
-              'FIM...')
+    #Professor
+        print("\n=============Professores================")
     elif opcao == 3:
-        print("------------Excluir-----------")
-        print('EM DESENVOLVIMENTO.......\n'
-              'FIM...')
+    #Disciplina
+        print("\n=============Disciplina================")
     elif opcao == 4:
-        print("------------Alterar-----------")
-        print('EM DESENVOLVIMENTO.......\n'
-              'FIM...')
-    elif opcao == 0:
-        print("------------Menu inicial-----------")
-        print('EM DESENVOLVIMENTO.......\n'
-              'FIM...')
+    #Turma
+        print("\n=============Turmas================")
+    elif opcao == 5:
+    #Matricula
+        print("\n=============Matriculas================")
     else:
-        print("FIM")    
+        break
+
     
-else: 
-    print("FIM")
+    op=1
+    while op !=0:
+
+        print('Escolha uma da opções abaixo:\n' 
+                ' 1) Incluir\n'
+                ' 2) Listar\n'
+                ' 3) Excluir\n'
+                ' 4) Alterar\n'
+                ' 0) Menu inicial\n')
+        op = int(input())
+        if op == 1:
+            print("------------INCLUIR-----------")
+            print("EM DESENVOLVIMENTO\n")
+        elif op == 2:
+            print("------------LIstar-----------")
+            print("EM DESENVOLVIMENTO\n")
+        elif op == 3:
+            print("------------Excluir-----------")
+            print("EM DESENVOLVIMENTO\n")
+        elif op == 4:
+            print("------------Alterar-----------")
+            print("EM DESENVOLVIMENTO\n")
+        
+        
